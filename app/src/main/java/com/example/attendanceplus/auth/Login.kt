7d551ddd56b47135266b9d01691b5f0318aae454
maxslimb <kpatel3032@gmail.com>
2021-04-04 19:25:25 +0530

    Sign up implemented

--- app/src/main/java/com/example/attendanceplus/auth/Login.kt
+++ app/src/main/java/com/example/attendanceplus/auth/Login.kt
@@ -1,11 +1,12 @@
-package com.example.attendanceplus
+package com.example.attendanceplus.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.attendanceplus.R
 
-class signup_screen : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup_screen)
+        setContentView(R.layout.activity_login_screen)
     }
 }
